a0190890aecb678a67b3edb3c7150d318206c0af
Jeremy Shen <ly0koS@hotmail.com>
2020-06-24 23:50:11 +0800

    Delete wrong string

--- USER/main.c
+++ USER/main.c
@@ -5,13 +5,7 @@
 #include "timer.h"
 #include "lcd.h"
 
-//ALIENTEK 探索者STM32F407开发板 实验10
-//输入捕获实验 -库函数版本
-//技术支持：www.openedv.com
-//淘宝店铺：http://eboard.taobao.com  
-//广州市星翼电子科技有限公司  
-//作者：正点原子 @ALIENTEK
- 
+
  
    				
 extern uint32_t Freq_InputCapture;
@@ -20,20 +14,20 @@ extern uint32_t Freq_InputCapture;
 int main(void)
 { 
 	u8 freq[8];
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置系统中断优先级分组2
-	delay_init(168);  //初始化延时函数
-	uart_init(115200);//初始化串口波特率为115200
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//锟斤拷锟斤拷系统锟叫讹拷锟斤拷锟饺硷拷锟斤拷锟斤拷2
+	delay_init(168);  //锟斤拷始锟斤拷锟斤拷时锟斤拷锟斤拷
+	uart_init(115200);//锟斤拷始锟斤拷锟斤拷锟节诧拷锟斤拷锟斤拷为115200
 	LCD_Init();
 	LCD_Display_Dir(1);	
 	POINT_COLOR=RED;
 	LCD_Clear(WHITE);
-// 	TIM14_PWM_Init(500-1,84-1);       	//84M/84=1Mhz的计数频率计数到500,PWM频率为1M/500=2Khz     
- 	TIM5_CH1_Cap_Init(0XFFFFFFFF,0); //以1Mhz的频率计数 
+// 	TIM14_PWM_Init(500-1,84-1);       	//84M/84=1Mhz锟侥硷拷锟斤拷频锟绞硷拷锟斤拷锟斤拷500,PWM频锟斤拷为1M/500=2Khz     
+ 	TIM5_CH1_Cap_Init(0XFFFFFFFF,0); //锟斤拷1Mhz锟斤拷频锟绞硷拷锟斤拷 
    	while(1)
 	{
 				sprintf((char*)freq,"Freq:%-7d Hz",Freq_InputCapture);
 				POINT_COLOR=BLUE;
-				printf("HIGH:%d Hz\r\n",Freq_InputCapture); //打印总的高点平时间
+				printf("HIGH:%d Hz\r\n",Freq_InputCapture); //锟斤拷印锟杰的高碉拷平时锟斤拷
 				LCD_ShowString(250,250,400,24,24,freq);
 		
 	}
